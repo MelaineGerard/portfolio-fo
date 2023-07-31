aaa39ed0aa68b1488e4027f3c1e0b8f5ee829dbc
Melaine Gérard <melaine.gerard@additi.fr>
2023-07-31 14:06:00 +0200

    :sparkles: Début de la création de la navbar

--- components/WebsiteNavbar.vue
+++ components/WebsiteNavbar.vue
@@ -1,5 +1,84 @@
 <template>
-  <div>
-    TODO: Navbar
-  </div>
+  <nav>
+    <a class="website-name" href="#">Melaine Gérard</a>
+    <ul>
+      <li>
+        <a href="#">
+          Accueil
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          Projets
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          Expériences professionnelles
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          Contact
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<style lang="scss" scoped>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #e7e7e7;
+    padding: 0.25rem 1rem;
+
+    ul {
+      display: flex;
+      list-style: none;
+
+      li {
+        margin-left: 1rem;
+
+        a {
+          text-decoration: none;
+          color: #000;
+          transition: all 0.2s ease-in-out;
+          font-size: 1.2rem;
+
+          &:hover {
+            color: #0053a7;
+          }
+        }
+      }
+    }
+
+    .website-name {
+      font-weight: bold;
+      font-size: 1.7rem;
+      text-decoration: none;
+      color: #000;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    nav {
+      flex-direction: column;
+      padding: 0.5rem 0;
+
+      ul {
+        flex-direction: column;
+        text-align: center;
+
+        li {
+          margin: 0.5rem 0;
+        }
+      }
+
+      .website-name {
+        text-align: center;
+      }
+    }
+  }
+</style>
