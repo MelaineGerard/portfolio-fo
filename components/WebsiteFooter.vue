c75e03a456e71b8d6562eb241f840eb6c83544ce
Melaine Gérard <melaine.gerard@additi.fr>
2023-08-01 08:55:42 +0200

    :sparkles: Ajout d'un footer

--- components/WebsiteFooter.vue
+++ components/WebsiteFooter.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    TODO: Footer
-  </div>
+  <footer>
+    Fait avec ❤️ par <a href="https://melaine-gerard.fr">Melaine Gérard</a>
+  </footer>
 </template>
+
+<style lang="scss" scoped>
+@import "assets/css/colors";
+  footer {
+    text-align: center;
+    padding: 1rem;
+    font-size: 1rem;
+    color: $black;
+    background: $background-secondary;
+
+    a {
+      color: $black;
+      text-decoration: none;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        color: $color-hover;
+      }
+    }
+  }
+</style>
