fc181e12e032268b1026831c3287a116ce803bc0
Melaine Gérard <melaine.gerard@additi.fr>
2023-08-07 08:59:00 +0200

    :sparkles: Début de l'implémentation de la partie expériences professionnelles

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,12 @@
   <div class="px-8">
     <WebsiteTopHomepage />
     <WebsiteAboutHomepage />
+    <WebsiteProfessionalExperience />
   </div>
 </template>
 
 <script setup lang="ts">
 import WebsiteTopHomepage from "~/components/homepage/WebsiteTopHomepage.vue";
 import WebsiteAboutHomepage from "~/components/homepage/WebsiteAboutHomepage.vue";
+import WebsiteProfessionalExperience from "~/components/homepage/WebsiteProfessionalExperience.vue";
 </script>
